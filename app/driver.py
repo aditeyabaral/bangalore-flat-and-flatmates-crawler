11f361fb1be25d86e75b89439ed17180535cef8d
Aditeya <aditeya.baral@gmail.com>
2022-10-16 00:19:14 +0530

    Add error handling

--- app/driver.py
+++ app/driver.py
@@ -1,12 +1,14 @@
 import os
 import time
 from selenium import webdriver
+import selenium
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 
 class Driver:
     def __init__(self):
@@ -49,7 +51,7 @@ class Driver:
 
     # TODO: Return create_time
     def parse_header_element(self, header_element):
-        anchor_tags = header_element.find_elements(By.XPATH, ".//a[@role=\"link\"]")
+        anchor_tags = header_element.find_elements(By.XPATH, './/a[@role="link"]')
         links = set()
         for anchor_tag in anchor_tags:
             try:
@@ -68,39 +70,39 @@ class Driver:
         return links
 
     def parse_body_element(self, body_element):
-        buttons = body_element.find_elements(By.XPATH, ".//div[@role=\"button\"]")
+        buttons = body_element.find_elements(By.XPATH, './/div[@role="button"]')
         for button in buttons:
             if button.get_attribute("aria-label") != "Message":
                 button.click()
-        paragraph_elements = body_element.find_elements(By.XPATH, ".//div[@dir=\"auto\"]")
+        paragraph_elements = body_element.find_elements(By.XPATH, './/div[@dir="auto"]')
         content = str()
         for paragraph_element in paragraph_elements:
-            content += paragraph_element.text.strip() + '\n'
+            content += paragraph_element.text.strip() + "\n"
         return content
-        
+
     def open_facebook(self):
         self.chrome.get("https://www.facebook.com/login/")
 
     def login_facebook(self, username, password):
         if self.chrome.current_url != "https://www.facebook.com/login/":
-            raise("Facebook is not open. Use open_facebook() to open Facebook first.")
+            raise ("Facebook is not open. Use open_facebook() to open Facebook first.")
 
         WebDriverWait(self.chrome, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[@id=\"email\"]"))
+            EC.presence_of_element_located((By.XPATH, '//*[@id="email"]'))
         )
-        email_input = self.chrome.find_element(By.XPATH, "//*[@id=\"email\"]")
-        password_input = self.chrome.find_element(By.XPATH, "//*[@id=\"pass\"]")
+        email_input = self.chrome.find_element(By.XPATH, '//*[@id="email"]')
+        password_input = self.chrome.find_element(By.XPATH, '//*[@id="pass"]')
         email_input.send_keys(username)
         password_input.send_keys(password)
         self.chrome.find_element(By.XPATH, "//button[@type='submit']").click()
-    
+
     def visit_group(self, group_name):
         time.sleep(3)
         url = f"https://www.facebook.com/groups/{group_name}?sorting_setting=CHRONOLOGICAL_LISTINGS"
         self.chrome.get(url)
 
     def scrape_posts_on_page(self):
-        # time.sleep(4)
+        time.sleep(2)
         for _ in range(10):
             ActionChains(self.chrome).send_keys(Keys.PAGE_DOWN).perform()
             time.sleep(1)
@@ -108,15 +110,28 @@ class Driver:
 
         feed_xpath = "/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[4]/div/div/div[2]/div/div/div[1]/div[2]/div[3]"
         feed_element = self.chrome.find_element(By.XPATH, feed_xpath)
-        post_elements = feed_element.find_elements(By.XPATH, "./div")[2:]
+        post_elements = feed_element.find_elements(By.XPATH, "./div")[1:]
         for post_element in post_elements:
-            content = str()
-            current_post_elements = post_element.find_elements(By.XPATH, "./div/div/div/div/div/div/div/div/div/div")[1:]
-            current_post_elements = list(filter(lambda x: x.get_attribute("class").strip() == "", current_post_elements))
-            current_post_elements = current_post_elements[0].find_elements(By.XPATH, "./div/div")
-            _, header_element, body_element, comment_element = current_post_elements
-            links = self.parse_header_element(header_element)
-            content = self.parse_body_element(body_element)
-            print(content)
-            print(links)
-            print('#' * 90)
+            try:
+                content = str()
+                current_post_elements = post_element.find_elements(
+                    By.XPATH, "./div/div/div/div/div/div/div/div/div/div"
+                )[1:]
+                current_post_elements = list(
+                    filter(
+                        lambda x: x.get_attribute("class").strip() == "",
+                        current_post_elements,
+                    )
+                )
+                current_post_elements = current_post_elements[0].find_elements(
+                    By.XPATH, "./div/div"
+                )
+                _, header_element, body_element, comment_element = current_post_elements
+                links = self.parse_header_element(header_element)
+                content = self.parse_body_element(body_element)
+                print(content)
+                print(links)
+                print("#" * 120)
+            except Exception as e:
+                print(f"Exception while parsing post: {e}")
+                pass
